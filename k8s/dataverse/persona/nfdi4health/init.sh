c09f149fb4b51c10f0af956a6fb51b6e5c6dd312
darms <darms@zbmed.de>
2023-07-14 15:24:50 +0200

    fix(k8s.dataverse): Update dataverse init script

--- k8s/dataverse/persona/nfdi4health/init.sh
+++ k8s/dataverse/persona/nfdi4health/init.sh
@@ -32,18 +32,18 @@ TSVS=$(find "${LICENCE_PATH}" -maxdepth 1 -iname 'license-*.json')
 # Load licences
 while IFS= read -r TSV; do
   echo -n "Loading ${TSV}: "
-  curl -X POST -H "Content-type: application/json"  $DATAVERSE_URL/api/licenses --upload-file ${TSV}
+  curl -H "X-Dataverse-key:$API_TOKEN" -X POST -H "Content-type: application/json"  $DATAVERSE_URL/api/licenses --upload-file ${TSV}
 done <<< "${TSVS}"
 
 echo "Add dataset permissions admin role"
-curl -X POST -H "Content-type:application/json" $DATAVERSE_URL/api/admin/roles --upload-file $SELF_LOCATION/dataset-permissions-admin.json
+curl -H "X-Dataverse-key:$API_TOKEN" -X POST -H "Content-type:application/json" $DATAVERSE_URL/api/admin/roles --upload-file $SELF_LOCATION/dataset-permissions-admin.json
 
 echo "Create dataverses"
 # Find all JSON files
 DATAVERSES=$(find $DATAVERSES_PATH -maxdepth 1 -iname '*.json')
 # Create dataverses
 echo -n "Publishing dataverse root:"
-curl -X POST $DATAVERSE_URL/api/dataverses/root/actions/:publish
+curl -H "X-Dataverse-key:$API_TOKEN" -X POST $DATAVERSE_URL/api/dataverses/root/actions/:publish
 
 while IFS= read -r DATAVERSE; do
   if [[ $(dirname "$DATAVERSE") -ef $DATAVERSES_PATH ]]; then
@@ -54,16 +54,16 @@ while IFS= read -r DATAVERSE; do
 
   DATAVERSE_ID=$(jq -r '.alias' $DATAVERSE)
   echo -n "Creating dataverse $PARENT_DATAVERSE/$DATAVERSE_ID:"
-  curl -X POST -H "Content-Type: application/json" $DATAVERSE_URL/api/dataverses/$PARENT_DATAVERSE --upload-file $DATAVERSE
+  curl -H "X-Dataverse-key:$API_TOKEN" -X POST -H "Content-Type: application/json" $DATAVERSE_URL/api/dataverses/$PARENT_DATAVERSE --upload-file $DATAVERSE
 
   echo -n "Publishing dataverse $PARENT_DATAVERSE/$DATAVERSE_ID:"
-  curl -X POST $DATAVERSE_URL/api/dataverses/$DATAVERSE_ID/actions/:publish
+  curl -H "X-Dataverse-key:$API_TOKEN" -X POST $DATAVERSE_URL/api/dataverses/$DATAVERSE_ID/actions/:publish
 
   echo -n "Adding dataverseAdmin as admin to dataverse $PARENT_DATAVERSE/$DATAVERSE_ID:"
-  curl -X POST -H "Content-Type: application/json" $DATAVERSE_URL/api/dataverses/$DATAVERSE_ID/assignments -d '{"assignee": "@dataverseAdmin", "role": "admin"}'
+  curl -H "X-Dataverse-key:$API_TOKEN" -X POST -H "Content-Type: application/json" $DATAVERSE_URL/api/dataverses/$DATAVERSE_ID/assignments -d '{"assignee": "@dataverseAdmin", "role": "admin"}'
 
   echo -n "Adding :authenticated-users as dataset creators to dataverse $PARENT_DATAVERSE/$DATAVERSE_ID:"
-  curl -X POST -H "Content-Type: application/json" $DATAVERSE_URL/api/dataverses/$DATAVERSE_ID/assignments -d '{"assignee": ":authenticated-users", "role": "dsContributor"}'
+  curl -H "X-Dataverse-key:$API_TOKEN" -X POST -H "Content-Type: application/json" $DATAVERSE_URL/api/dataverses/$DATAVERSE_ID/assignments -d '{"assignee": ":authenticated-users", "role": "dsContributor"}'
 done <<< "${DATAVERSES}"
 
 # Last step as existence of one block is the indicator for a complete bootstrapped installation
