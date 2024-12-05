a37c24321b64b5124c4dab52edd213cc914c42f2
Vera Clemens <clemens@zbmed.de>
2024-12-05 16:47:15 +0100

    feat(k8s.dataverse): add Solr backup loading to script

--- scripts/load_dataverse_backup.sh
+++ scripts/load_dataverse_backup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+LOGICAL_BACKUP_S3_BUCKET=
+SCOPE=
+LOGICAL_BACKUP_S3_BUCKET_SCOPE_SUFFIX=
+S3_CONFIG_FILE=
+SOURCE_DATAVERSE_NAME=
+SOURCE_DATAVERSE_CONTEXT=
+DESTINATION_DATAVERSE_NAME=
+DESTINATION_DATAVERSE_CONTEXT=
+
 S3_CONFIG_FILE="${S3_CONFIG_FILE:-'~/.s3cfg'}"
 
 # Computed from env variables above
@@ -7,55 +16,89 @@ LAST_BACKUP_FILE=$(s3cmd ls s3://$LOGICAL_BACKUP_S3_BUCKET/spilo/$SCOPE$LOGICAL_
 POSTGRES_POD_NAME=${DESTINATION_DATAVERSE_NAME}-dataverse-postgres-0
 DATAVERSE_POD_NAME=${DESTINATION_DATAVERSE_NAME}-dataverse-0
 
-echo "Downloading backup from S3..."
-s3cmd get $LAST_BACKUP_FILE . -c $S3_CONFIG_FILE --skip-existing
-
-echo "Copying backup to postgres pod..."
-kubectl cp $(basename $LAST_BACKUP_FILE) $POSTGRES_POD_NAME:/tmp/
-
-echo "Unzipping backup..."
-kubectl exec $POSTGRES_POD_NAME -- gunzip /tmp/$(basename $LAST_BACKUP_FILE)
-
-echo "Emptying database..."
-kubectl exec $POSTGRES_POD_NAME -- psql -P pager=off -U dataverse -c "-- Recreate the schema
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-
--- Restore default permissions
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;"
-# source: https://stackoverflow.com/a/61221726
-
-echo "Loading backup into database..."
-kubectl exec $POSTGRES_POD_NAME -- psql -P pager=off -U dataverse -f /tmp/$(basename $LAST_BACKUP_FILE .gz) template1
-
-
-echo "Updating database passwords..."
-kubectl get secret | grep ${DESTINATION_DATAVERSE_NAME}-dataverse-postgres.credentials.postgresql.acid.zalan.do | awk '{print $1}' | while read SECRET; do kubectl exec $POSTGRES_POD_NAME -- psql -P pager=off -U dataverse -c "ALTER USER $(echo $SECRET | awk -F. '{print $1}') WITH PASSWORD '$(kubectl get secrets/$SECRET -o=jsonpath="{.data.password}" | base64 -d)';"; done
-
-echo "Restarting dataverse pod..."
-kubectl delete pod $DATAVERSE_POD_NAME
-kubectl wait --for=condition=Ready --timeout=-1s pod/$DATAVERSE_POD_NAME
-
+#echo "Downloading backup from S3..."
+#s3cmd get $LAST_BACKUP_FILE . -c $S3_CONFIG_FILE --skip-existing
+#
+#echo "Copying backup to postgres pod..."
+#kubectl cp $(basename $LAST_BACKUP_FILE) $POSTGRES_POD_NAME:/tmp/ --context $DESTINATION_DATAVERSE_CONTEXT
+#
+#echo "Unzipping backup..."
+#kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -- gunzip /tmp/$(basename $LAST_BACKUP_FILE)
+#
+#echo "Emptying database..."
+#kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -- psql -P pager=off -U dataverse -c "-- Recreate the schema
+#DROP SCHEMA public CASCADE;
+#CREATE SCHEMA public;
+#
+#-- Restore default permissions
+#GRANT ALL ON SCHEMA public TO postgres;
+#GRANT ALL ON SCHEMA public TO public;"
+## source: https://stackoverflow.com/a/61221726
+#
+#echo "Loading backup into database..."
+#kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -- psql -P pager=off -U dataverse -f /tmp/$(basename $LAST_BACKUP_FILE .gz) template1
+#
+#
+#echo "Updating database passwords..."
+#kubectl get secret --context $DESTINATION_DATAVERSE_CONTEXT | grep ${DESTINATION_DATAVERSE_NAME}-dataverse-postgres.credentials.postgresql.acid.zalan.do | awk '{print $1}' | while read SECRET; do kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -- psql -P pager=off -U dataverse -c "ALTER USER $(echo $SECRET | awk -F. '{print $1}') WITH PASSWORD '$(kubectl get secrets/$SECRET -o=jsonpath="{.data.password}" --context $DESTINATION_DATAVERSE_CONTEXT | base64 -d)';"; done
+#
+#echo "Restarting dataverse pod..."
+#kubectl delete pod $DATAVERSE_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT
+#kubectl wait --for=condition=Ready --timeout=-1s --context $DESTINATION_DATAVERSE_CONTEXT pod/$DATAVERSE_POD_NAME
+#
+## NOTE: The following block is commented out because it's no longer feasible time-wise to reindex Dataverse after
+# loading a backup. Since we have over 25,000 datasets, it takes too long.
+# Instead, we also load a backup for the Solr index.
 # Using port 8081 because 8080 is often already used if currently developing with Dataverse
-DATAVERSE_LOCAL_PORT=8081
-DATAVERSE_REMOTE_PORT=8080
+#DATAVERSE_LOCAL_PORT=8086
+#DATAVERSE_REMOTE_PORT=8080
+#
+#echo "Starting re-index..."
+#kubectl port-forward $DATAVERSE_POD_NAME $DATAVERSE_LOCAL_PORT:$DATAVERSE_REMOTE_PORT >/dev/null &
+#PORT_FORWARD_PID=$!
+## Kill the port-forward when this script exits
+#trap '{
+#    kill $PORT_FORWARD_PID 2>/dev/null
+#}' EXIT
+## Wait for port to be available
+#while ! nc -vz localhost $DATAVERSE_LOCAL_PORT >/dev/null 2>&1; do
+#    sleep 0.1
+#done
+#curl http://localhost:$DATAVERSE_LOCAL_PORT/api/admin/index/clear
+#echo
+#curl http://localhost:$DATAVERSE_LOCAL_PORT/api/admin/index
+#echo
 
-echo "Starting re-index..."
-kubectl port-forward $DATAVERSE_POD_NAME $DATAVERSE_LOCAL_PORT:$DATAVERSE_REMOTE_PORT >/dev/null &
-PORT_FORWARD_PID=$!
-# Kill the port-forward when this script exits
-trap '{
-    kill $PORT_FORWARD_PID 2>/dev/null
-}' EXIT
-# Wait for port to be available
-while ! nc -vz localhost $DATAVERSE_LOCAL_PORT >/dev/null 2>&1; do
-    sleep 0.1
+echo "Creating backup of source Solr..."
+kubectl exec -it ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --context $SOURCE_DATAVERSE_CONTEXT --container solr -- curl -s "localhost:8983/solr/collection1/replication?command=backup&numberToKeep=1" > /dev/null
+while true; do
+    SOLR_BACKUP_RESPONSE=$(kubectl exec -it ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --context $SOURCE_DATAVERSE_CONTEXT --container solr -- curl localhost:8983/solr/collection1/replication?command=details)
+    SOLR_BACKUP_STATUS=$(echo $SOLR_BACKUP_RESPONSE | jq -r '.details.backup.status')
+    if [[ "$SOLR_BACKUP_STATUS" == "success" ]]; then
+        break
+    fi
+    echo "Waiting for Solr backup to be completed..."
+    sleep 1
 done
-curl http://localhost:$DATAVERSE_LOCAL_PORT/api/admin/index/clear
-echo
-curl http://localhost:$DATAVERSE_LOCAL_PORT/api/admin/index
-echo
+
+echo "Copying completed backup to destination Solr... (this may take some time)"
+SOLR_BACKUP_NAME=$(echo $SOLR_BACKUP_RESPONSE | jq -r '.details.backup.directoryName')
+kubectl exec -it ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $SOURCE_DATAVERSE_CONTEXT -- tar -zcf /tmp/${SOLR_BACKUP_NAME}.tar.gz /var/solr/data/collection1/data/${SOLR_BACKUP_NAME}
+kubectl cp ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0:/tmp/${SOLR_BACKUP_NAME}.tar.gz ${SOLR_BACKUP_NAME}.tar.gz --container solr --context $SOURCE_DATAVERSE_CONTEXT --retries=-1 > /dev/null
+kubectl cp ${SOLR_BACKUP_NAME}.tar.gz ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0:/tmp/ --container solr --context $DESTINATION_DATAVERSE_CONTEXT --retries=-1 > /dev/null
+kubectl exec -it ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $DESTINATION_DATAVERSE_CONTEXT -- tar -zxf /tmp/${SOLR_BACKUP_NAME}.tar.gz -C /var/solr/data/collection1/data/
+
+kubectl exec -it ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --context $DESTINATION_DATAVERSE_CONTEXT --container solr -- curl -s localhost:8983/solr/collection1/replication?command=restore > /dev/null
+
+while true; do
+    SOLR_BACKUP_LOAD_RESPONSE=$(kubectl exec -it ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --context $DESTINATION_DATAVERSE_CONTEXT --container solr -- curl localhost:8983/solr/collection1/replication?command=restorestatus)
+    SOLR_BACKUP_LOAD_STATUS=$(echo $SOLR_BACKUP_RESPONSE | jq -r '.restorestatus.status')
+    if [[ "$SOLR_BACKUP_LOAD_STATUS" == "success" ]]; then
+        break
+    fi
+    echo "Waiting for Solr backup to be loaded..."
+    sleep 5
+done
 
 echo
-echo "Done! Please wait for the re-indexing to finish, then the backup loading will be complete."
+echo "Done! Backup loading complete."
