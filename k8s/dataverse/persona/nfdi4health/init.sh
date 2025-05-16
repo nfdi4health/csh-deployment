91ef514a00feabfb29f77fb0a0c14ad6d31ecc17
Vera Clemens <clemens@zbmed.de>
2025-05-16 16:15:46 +0200

    fix(k8s.dataverse): don't fail init script when installation name is not set

--- k8s/dataverse/persona/nfdi4health/init.sh
+++ k8s/dataverse/persona/nfdi4health/init.sh
@@ -77,9 +77,11 @@ while IFS= read -r ROLE; do
   echo
 done <<< "${ROLES}"
 
-echo "Updating root dataverse name"
-curl -X PUT "$DATAVERSE_URL/api/dataverses/root/attribute/name?value=$DATAVERSE_INSTALLATION_NAME"
-echo
+if [ -z "$DATAVERSE_INSTALLATION_NAME" ]; then
+    echo "Updating root dataverse name"
+    curl -X PUT "$DATAVERSE_URL/api/dataverses/root/attribute/name?value=$DATAVERSE_INSTALLATION_NAME"
+    echo
+fi
 
 echo "Create dataverses"
 # NOTE Using POSIX C locale to force sorting by simple byte comparison. This sorts "." before "_". This is to ensure
