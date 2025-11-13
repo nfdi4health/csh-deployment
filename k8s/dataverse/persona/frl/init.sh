547570e659949283419698ef9d4e262e8c6a2f70
Vera Clemens <clemens@zbmed.de>
2025-11-13 17:49:57 +0100

    feat(k8s.dataverse): add FRL persona

---
+++ k8s/dataverse/persona/frl/init.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -euo pipefail
+
+# Set some defaults as documented
+DATAVERSE_URL=${DATAVERSE_URL:-"http://dataverse:8080"}
+export DATAVERSE_URL
+
+# get current dir location
+SELF_LOCATION=${BOOTSTRAP_DIR}/${PERSONA}/
+echo "SELF_LOCATION = $SELF_LOCATION"
+
+echo "Running base setup-all.sh (INSECURE MODE, securing later)"
+"${BOOTSTRAP_DIR}"/base/setup-all.sh --insecure -p=admin1 | tee /tmp/setup-all.sh.out
+API_TOKEN=$(grep apiToken "/tmp/setup-all.sh.out" | jq ".data.apiToken" | tr -d \")
+export API_TOKEN
+
+# configure curl
+echo "# hide progress meter
+-s
+# authentication
+-H \"X-Dataverse-key:$API_TOKEN\"
+# fail script on server error
+--fail-with-body" > ~/.curlrc
+
+if [ -f "${DATAVERSES_PATH}/root.json" ]; then
+    echo "Updating root collection"
+    curl -X PUT "$DATAVERSE_URL/api/dataverses/root" --upload-file ${DATAVERSES_PATH}/root.json
+    echo
+fi
+
+echo "Publishing root collection"
+curl -X POST "${DATAVERSE_URL}/api/dataverses/:root/actions/:publish"
+echo
+
+if [ -f "${CONFIG_PATH}/cvoc_conf.json" ]; then
+    echo "Configuring external vocabularies"
+    curl -X PUT "$DATAVERSE_URL/api/admin/settings/:CVocConf" --upload-file ${CONFIG_PATH}/cvoc_conf.json
+    echo
+fi
+
+echo "Setting up multiple languages"
+curl -X PUT $DATAVERSE_URL/api/admin/settings/:Languages -d '[{"locale":"en","title":"English"},{"locale":"de","title":"Deutsch"}]'
+echo
+
+# Last step as existence of one block is the indicator for a complete bootstrapped installation
+echo "Load custom metadata blocks"
+# Find all TSV files
+TSVS=$(find "${METADATABLOCKS_PATH}" -maxdepth 1 -iname '*.tsv')
+# Load metadata blocks
+while IFS= read -r TSV; do
+  echo "Loading ${TSV}:"
+  curl -X POST -H "Content-type: text/tab-separated-values" $DATAVERSE_URL/api/admin/datasetfield/load --upload-file ${TSV}
+  echo
+done <<< "${TSVS}"
+
+echo "Activating metadata blocks"
+curl -X POST -H "Content-Type: application/json" $DATAVERSE_URL/api/dataverses/:root/metadatablocks -d "[\"citation\",\"geospatial\"]"
+echo
+
+echo "Restricting sensitive API endpoints..."
+curl -X DELETE "${DATAVERSE_URL}/api/admin/settings/BuiltinUsers.KEY"
+curl -X PUT "${DATAVERSE_URL}/api/admin/settings/:BlockedApiEndpoints" -d 'admin,builtin-users'
+echo
+
+echo
+echo
+echo "...DONE!"
