384ec37202f842ff2a24e0690563c5c625763d83
Vera Clemens <clemens@zbmed.de>
2026-01-28 10:24:53 +0100

    feat(scripts): add namespace support to load_dataverse_backup.sh

--- scripts/load_dataverse_backup.sh
+++ scripts/load_dataverse_backup.sh
@@ -6,8 +6,10 @@ LOGICAL_BACKUP_S3_BUCKET_SCOPE_SUFFIX=
 S3_CONFIG_FILE=
 SOURCE_DATAVERSE_NAME=
 SOURCE_DATAVERSE_CONTEXT=
+SOURCE_DATAVERSE_NAMESPACE=
 DESTINATION_DATAVERSE_NAME=
 DESTINATION_DATAVERSE_CONTEXT=
+DESTINATION_DATAVERSE_NAMESPACE=
 
 S3_CONFIG_FILE="${S3_CONFIG_FILE:-'~/.s3cfg'}"
 
@@ -20,13 +22,13 @@ echo "Downloading backup from S3..."
 s3cmd get $LAST_BACKUP_FILE . -c $S3_CONFIG_FILE --skip-existing
 
 echo "Copying backup to postgres pod..."
-kubectl cp $(basename $LAST_BACKUP_FILE) $POSTGRES_POD_NAME:/tmp/ --context $DESTINATION_DATAVERSE_CONTEXT
+kubectl cp $(basename $LAST_BACKUP_FILE) $POSTGRES_POD_NAME:/tmp/ --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE
 
 echo "Unzipping backup..."
-kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -- gunzip /tmp/$(basename $LAST_BACKUP_FILE)
+kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE -- gunzip /tmp/$(basename $LAST_BACKUP_FILE)
 
 echo "Emptying database..."
-kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -- psql -P pager=off -U dataverse -c "-- Recreate the schema
+kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE -- psql -P pager=off -U dataverse -c "-- Recreate the schema
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
@@ -36,15 +38,15 @@ GRANT ALL ON SCHEMA public TO public;"
 # source: https://stackoverflow.com/a/61221726
 
 echo "Loading backup into database..."
-kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -- psql -P pager=off -U dataverse -f /tmp/$(basename $LAST_BACKUP_FILE .gz) template1
+kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE -- psql -P pager=off -U dataverse -f /tmp/$(basename $LAST_BACKUP_FILE .gz) template1
 
 
 echo "Updating database passwords..."
-kubectl get secret --context $DESTINATION_DATAVERSE_CONTEXT | grep ${DESTINATION_DATAVERSE_NAME}-dataverse-postgres.credentials.postgresql.acid.zalan.do | awk '{print $1}' | while read SECRET; do kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -- psql -P pager=off -U dataverse -c "ALTER USER $(echo $SECRET | awk -F. '{print $1}') WITH PASSWORD '$(kubectl get secrets/$SECRET -o=jsonpath="{.data.password}" --context $DESTINATION_DATAVERSE_CONTEXT | base64 -d)';"; done
+kubectl get secret --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE | grep ${DESTINATION_DATAVERSE_NAME}-dataverse-postgres.credentials.postgresql.acid.zalan.do | awk '{print $1}' | while read SECRET; do kubectl exec $POSTGRES_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE -- psql -P pager=off -U dataverse -c "ALTER USER $(echo $SECRET | awk -F. '{print $1}') WITH PASSWORD '$(kubectl get secrets/$SECRET -o=jsonpath="{.data.password}" --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE | base64 -d)';"; done
 
 echo "Restarting dataverse pod..."
-kubectl delete pod $DATAVERSE_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT
-kubectl wait --for=condition=Ready --timeout=-1s --context $DESTINATION_DATAVERSE_CONTEXT pod/$DATAVERSE_POD_NAME
+kubectl delete pod $DATAVERSE_POD_NAME --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE
+kubectl wait --for=condition=Ready --timeout=-1s --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE pod/$DATAVERSE_POD_NAME
 #
 ## NOTE: The following block is commented out because it's no longer feasible time-wise to reindex Dataverse after
 # loading a backup. Since we have over 25,000 datasets, it takes too long.
@@ -71,7 +73,7 @@ kubectl wait --for=condition=Ready --timeout=-1s --context $DESTINATION_DATAVERS
 
 need_to_create_solr_backup () {
     echo "Checking age of latest backup of source Solr..."
-    SOLR_BACKUP_RESPONSE=$(kubectl exec -it ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --context $SOURCE_DATAVERSE_CONTEXT --container solr -- curl localhost:8983/solr/collection1/replication?command=details)
+    SOLR_BACKUP_RESPONSE=$(kubectl exec -it ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --context $SOURCE_DATAVERSE_CONTEXT -n $SOURCE_DATAVERSE_NAMESPACE --container solr -- curl localhost:8983/solr/collection1/replication?command=details)
     SOLR_BACKUP_STATUS=$(echo $SOLR_BACKUP_RESPONSE | jq -r '.details.backup.status')
     if [[ "$SOLR_BACKUP_STATUS" == "success" ]]; then
         SOLR_BACKUP_TIMESTAMP=$(echo $SOLR_BACKUP_RESPONSE | jq -r '.details.backup.snapshotCompletedAt')
@@ -98,9 +100,9 @@ need_to_create_solr_backup () {
 
 create_solr_backup () {
     echo "Creating backup of source Solr..."
-    kubectl exec ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --context $SOURCE_DATAVERSE_CONTEXT --container solr -- curl -s "localhost:8983/solr/collection1/replication?command=backup&numberToKeep=1"; echo
+    kubectl exec ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --context $SOURCE_DATAVERSE_CONTEXT -n $SOURCE_DATAVERSE_NAMESPACE --container solr -- curl -s "localhost:8983/solr/collection1/replication?command=backup&numberToKeep=1"; echo
     while true; do
-        SOLR_BACKUP_RESPONSE=$(kubectl exec ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --context $SOURCE_DATAVERSE_CONTEXT --container solr -- curl localhost:8983/solr/collection1/replication?command=details)
+        SOLR_BACKUP_RESPONSE=$(kubectl exec ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --context $SOURCE_DATAVERSE_CONTEXT -n $SOURCE_DATAVERSE_NAMESPACE --container solr -- curl localhost:8983/solr/collection1/replication?command=details)
         SOLR_BACKUP_STATUS=$(echo $SOLR_BACKUP_RESPONSE | jq -r '.details.backup.status')
         if [[ "$SOLR_BACKUP_STATUS" == "success" ]]; then
             break
@@ -116,21 +118,21 @@ fi
 
 SOLR_BACKUP_NAME=$(echo $SOLR_BACKUP_RESPONSE | jq -r '.details.backup.directoryName')
 
-if kubectl exec ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $DESTINATION_DATAVERSE_CONTEXT -- sh -c "[ -d /var/solr/data/collection1/data/${SOLR_BACKUP_NAME} ]"; then
+if kubectl exec ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE -- sh -c "[ -d /var/solr/data/collection1/data/${SOLR_BACKUP_NAME} ]"; then
     echo "Backup was already copied to destination Solr."
 else
     echo "Copying completed backup ${SOLR_BACKUP_NAME} to destination Solr... (this may take some time)"
-    kubectl exec ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $SOURCE_DATAVERSE_CONTEXT -- tar -zcf /tmp/${SOLR_BACKUP_NAME}.tar.gz -C /var/solr/data/collection1/data/ ${SOLR_BACKUP_NAME} > /dev/null
-    kubectl cp ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0:/tmp/${SOLR_BACKUP_NAME}.tar.gz ${SOLR_BACKUP_NAME}.tar.gz --container solr --context $SOURCE_DATAVERSE_CONTEXT --retries=-1 > /dev/null
-    kubectl cp ${SOLR_BACKUP_NAME}.tar.gz ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0:/tmp/ --container solr --context $DESTINATION_DATAVERSE_CONTEXT --retries=-1 > /dev/null
-    kubectl exec ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $DESTINATION_DATAVERSE_CONTEXT -- rm -r /var/solr/data/collection1/data/snapshot.*
-    kubectl exec ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $DESTINATION_DATAVERSE_CONTEXT -- tar -zxf /tmp/${SOLR_BACKUP_NAME}.tar.gz -C /var/solr/data/collection1/data/
+    kubectl exec ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $SOURCE_DATAVERSE_CONTEXT -n $SOURCE_DATAVERSE_NAMESPACE -- tar -zcf /tmp/${SOLR_BACKUP_NAME}.tar.gz -C /var/solr/data/collection1/data/ ${SOLR_BACKUP_NAME} > /dev/null
+    kubectl cp ${SOURCE_DATAVERSE_NAME}-dataverse-solr-0:/tmp/${SOLR_BACKUP_NAME}.tar.gz ${SOLR_BACKUP_NAME}.tar.gz --container solr --context $SOURCE_DATAVERSE_CONTEXT -n $SOURCE_DATAVERSE_NAMESPACE --retries=-1 > /dev/null
+    kubectl cp ${SOLR_BACKUP_NAME}.tar.gz ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0:/tmp/ --container solr --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE --retries=-1 > /dev/null
+    kubectl exec ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE -- rm -r /var/solr/data/collection1/data/snapshot.*
+    kubectl exec ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --container solr --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE -- tar -zxf /tmp/${SOLR_BACKUP_NAME}.tar.gz -C /var/solr/data/collection1/data/
 fi
 
-kubectl exec ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --context $DESTINATION_DATAVERSE_CONTEXT --container solr -- curl -s "localhost:8983/solr/collection1/replication?command=restore" > /dev/null
+kubectl exec ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE --container solr -- curl -s "localhost:8983/solr/collection1/replication?command=restore" > /dev/null
 
 while true; do
-    SOLR_BACKUP_LOAD_RESPONSE=$(kubectl exec -t ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --context $DESTINATION_DATAVERSE_CONTEXT --container solr -- curl -s "localhost:8983/solr/collection1/replication?command=restorestatus")
+    SOLR_BACKUP_LOAD_RESPONSE=$(kubectl exec -t ${DESTINATION_DATAVERSE_NAME}-dataverse-solr-0 --context $DESTINATION_DATAVERSE_CONTEXT -n $DESTINATION_DATAVERSE_NAMESPACE --container solr -- curl -s "localhost:8983/solr/collection1/replication?command=restorestatus")
     SOLR_BACKUP_LOAD_STATUS=$(echo $SOLR_BACKUP_LOAD_RESPONSE | jq -r '.restorestatus.status')
     if [[ "$SOLR_BACKUP_LOAD_STATUS" == "success" ]]; then
         break
