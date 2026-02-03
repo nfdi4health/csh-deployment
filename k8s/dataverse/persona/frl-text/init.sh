9593ecd40e1635d1823081954bdab663715cb52c
Vera Clemens <clemens@zbmed.de>
2026-02-03 16:25:54 +0100

    feat(k8s.dataverse): add frl-text persona

--- k8s/dataverse/persona/frl-text/init.sh
+++ k8s/dataverse/persona/frl-text/init.sh
@@ -42,9 +42,9 @@ echo "Setting up multiple languages"
 curl -X PUT $DATAVERSE_URL/api/admin/settings/:Languages -d '[{"locale":"en","title":"English"},{"locale":"de","title":"Deutsch"}]'
 echo
 
-echo "Setting up external tools"
-"${SELF_LOCATION}"/init-external-tools.sh
-echo
+#echo "Setting up external tools"
+#"${SELF_LOCATION}"/init-external-tools.sh
+#echo
 
 echo "Enabling file-level embargoes"
 curl -X PUT $DATAVERSE_URL/api/admin/settings/:MaxEmbargoDurationInMonths -d -1
@@ -54,7 +54,7 @@ echo "Configuring dataset summary fields"
 curl -X PUT $DATAVERSE_URL/api/admin/settings/:CustomDatasetSummaryFields -d 'dsDescription,author,datasetContact,subject,keyword,publication,grantNumber,yearOfCopyright'
 echo
 
-# TODO Currently, the CSS file must be *manually* copied to /dv/branding and then configured via API
+# TODO Currently, the CSS file must be manually copied to /dv/branding
 #echo "Configuring custom CSS"
 #curl -X PUT -d '/dv/branding/style.css' http://localhost:8080/api/admin/settings/:StyleCustomizationFile
 #echo
