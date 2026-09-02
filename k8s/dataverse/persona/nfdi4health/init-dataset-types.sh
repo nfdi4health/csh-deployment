952cc9f9f20d46f8235b458334524b0baf0b7b9d
Vera Clemens <clemens@zbmed.de>
2026-09-02 15:08:18 +0200

    feat(k8s.dataverse): activate dataset types for all collections in nfdi4health persona

--- k8s/dataverse/persona/nfdi4health/init-dataset-types.sh
+++ k8s/dataverse/persona/nfdi4health/init-dataset-types.sh
@@ -1,5 +1,7 @@
+# Create dataset types
 curl -H "Content-Type: application/json" -X POST -d '{"name": "study", "displayName": "Study"}' $DATAVERSE_URL/api/datasets/datasetTypes
 curl -H "Content-Type: application/json" -X POST -d '{"name": "substudy", "displayName": "Substudy"}' $DATAVERSE_URL/api/datasets/datasetTypes
+# The type "dataset" already exists by default
 #curl -H "Content-Type: application/json" -X POST -d '{"name": "dataset", "displayName": "Dataset"}' $DATAVERSE_URL/api/datasets/datasetTypes
 curl -H "Content-Type: application/json" -X POST -d '{"name": "studyProtocol", "displayName": "Study protocol"}' $DATAVERSE_URL/api/datasets/datasetTypes
 curl -H "Content-Type: application/json" -X POST -d '{"name": "dataDictionary", "displayName": "Data dictionary"}' $DATAVERSE_URL/api/datasets/datasetTypes
@@ -21,3 +23,25 @@ curl -H "Content-Type: application/json" -X POST -d '{"name": "other", "displayN
 curl -H "Content-Type: application/json" -X POST -d '{"name": "registry", "displayName": "Registry"}' $DATAVERSE_URL/api/datasets/datasetTypes
 curl -H "Content-Type: application/json" -X POST -d '{"name": "secondaryDataSource", "displayName": "Secondary data source"}' $DATAVERSE_URL/api/datasets/datasetTypes
 curl -H "Content-Type: application/json" -X POST -d '{"name": "biobank", "displayName": "Biobank"}' $DATAVERSE_URL/api/datasets/datasetTypes
+
+# Activate dataset types for all collections
+ALLOWED_DATASET_TYPES="study,substudy,dataset,studyProtocol,dataDictionary,informedConsentForm,patientInformationSheet,manualOfOperationsSops,statisticalAnalysisPlan,dataManagementPlan,caseReportForm,codeBook,questionnaire,interviewSchemeAndThemes,observationGuide,discussionGuide,participantTasks,otherDataCollectionInstrument,otherStudyDocument,other,registry,secondaryDataSource,biobank"
+START=0
+
+while true; do
+  SEARCH_RESPONSE=$(curl "$DATAVERSE_URL/api/search?type=dataverse&q=*&start=$START")
+  TOTAL_COUNT=$(jq -r '.data.total_count' <<< "$SEARCH_RESPONSE")
+  COUNT_IN_RESPONSE=$(jq -r '.data.count_in_response' <<< "$SEARCH_RESPONSE")
+
+  while IFS= read -r DATAVERSE_ID; do
+    echo "Activating dataset types for dataverse $DATAVERSE_ID"
+    curl -X PUT "$DATAVERSE_URL/api/dataverses/$DATAVERSE_ID/attribute/allowedDatasetTypes?value=$ALLOWED_DATASET_TYPES"
+    echo
+  done < <(jq -r '.data.items[] | .identifier' <<< "$SEARCH_RESPONSE")
+
+  if (( COUNT_IN_RESPONSE == 0 || START + COUNT_IN_RESPONSE >= TOTAL_COUNT )); then
+    break
+  fi
+
+  START=$((START + COUNT_IN_RESPONSE))
+done
